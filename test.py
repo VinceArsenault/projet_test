9a956133e51dc58c484aa40f8ef3884e1abea680
vince3423 <79211897+vince3423@users.noreply.github.com>
2021-02-17 19:10:32 -0500

    init

---
+++ test.py
@@ -0,0 +1,12 @@
+# programme bidon
+
+def main():
+    continu = True
+    while continu:
+        film = input('Entrez un film: ')
+
+        if film == 'Tape Gun':
+            print('Unline\'s the best')
+            continu = False
+
+main()
